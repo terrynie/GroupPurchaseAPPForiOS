0ae9905080c5920d7ca5f159cca7474bc52b517e
terry408911 <519046658@qq.com>
2015-07-21 15:51:02 +0800

    read data from plist documents,and according to the data read from plist documents set the tableview

--- LaShouTuanGou/CityTableViewController.swift
+++ LaShouTuanGou/CityTableViewController.swift
@@ -10,38 +10,47 @@ import Foundation
 import UIKit
 
 class CityTableViewController:UITableViewController,UITableViewDataSource,UITableViewDelegate {
-    var sectionArray:NSMutableArray = NSMutableArray()
+    var sectionArray:NSMutableArray!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var cityArray1:NSMutableArray = NSMutableArray()
-        cityArray1.addObject("北京")
-        cityArray1.addObject("上海")
+//        var cityArray1:NSMutableArray = NSMutableArray()
+//        cityArray1.addObject("北京")
+//        cityArray1.addObject("上海")
+//        
+//        var cityArray2:NSMutableArray = NSMutableArray()
+//        cityArray2.addObject("安徽")
+//        cityArray2.addObject("江苏")
+//        cityArray2.addObject("河南")
+//        
+//        sectionArray.addObject(cityArray1)
+//        sectionArray.addObject(cityArray2)
         
-        var cityArray2:NSMutableArray = NSMutableArray()
-        cityArray2.addObject("安徽")
-        cityArray2.addObject("江苏")
-        cityArray2.addObject("河南")
         
-        sectionArray.addObject(cityArray1)
-        sectionArray.addObject(cityArray2)
+        var bundler:NSBundle = NSBundle.mainBundle()
+        //通过查找，返回文件的路径
+        var path:String = bundler.pathForResource("ChinaCity.plist", ofType: nil)!
+        self.sectionArray = NSMutableArray(contentsOfFile: path)!
+        
+       
         
     }
     
     //显示多少组数
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int{
         
-        
         return sectionArray.count
     }
     
     //每组多少行
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        var rowCount:Int
-        rowCount = self.sectionArray[section].count
-        return rowCount
+        
+        var cityMutableDictionary:NSMutableDictionary = self.sectionArray[section] as! NSMutableDictionary
+        var countryMutableArray:NSMutableArray = cityMutableDictionary["list"] as! NSMutableArray
+        //根据字典的长度确定每组的行数
+        return countryMutableArray.count
         
     }
     
@@ -49,12 +58,13 @@ class CityTableViewController:UITableViewController,UITableViewDataSource,UITabl
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         
         var cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cityID") as UITableViewCell
-//        //视频教程写法如下
-//        var sectionArray:NSMutableArray = self.sectionArray[indexPath.section] as! NSMutableArray;
-//        var cityArrayName :NSString = sectionArray[indexPath.row] as! String
-//        cell.textLabel?.text = cityArrayName as String
+        var cityMutableDictionary:NSMutableDictionary = self.sectionArray[indexPath.section] as! NSMutableDictionary
+        var countryMutableArray:NSMutableArray = cityMutableDictionary["list"] as! NSMutableArray
+        var countryMutableDictionary:NSMutableDictionary = countryMutableArray[indexPath.row] as! NSMutableDictionary
         
-        cell.textLabel?.text = self.sectionArray[indexPath.section][indexPath.row] as? String
+        cell.textLabel?.text = countryMutableDictionary["name"] as? String
+        
+        
         
         return cell
         
