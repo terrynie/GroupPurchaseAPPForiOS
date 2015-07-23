d643917b7057ab47157ac92aacf170e448081adb
terry408911 <519046658@qq.com>
2015-07-23 15:27:33 +0800

    load the .xib file,which we customized, on thescreen

--- LaShouTuanGou/ProductViewController.swift
+++ LaShouTuanGou/ProductViewController.swift
@@ -23,6 +23,8 @@ class ProductViewController: UIViewController,UITableViewDataSource,UITableViewD
         //因为本类是UIViewController，不能实现dalegate和datasource接口，所以通过这种方式进行实现
         self.productTableView.delegate = self
         self.productTableView.dataSource = self
+        //初始化时设置tableview的高度
+        self.productTableView.rowHeight = 80
     }
     
     override func didReceiveMemoryWarning() {
@@ -55,9 +57,6 @@ class ProductViewController: UIViewController,UITableViewDataSource,UITableViewD
     }
     
     
-    
-    
-    
     //每组有多少个cell
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         
@@ -69,23 +68,35 @@ class ProductViewController: UIViewController,UITableViewDataSource,UITableViewD
         var productID:String = "productID"
         
         //在缓冲区查找是否存在productID标识cell
-        var cell:UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(productID) as? UITableViewCell
+        var cell:ProductCell? = tableView.dequeueReusableCellWithIdentifier(productID) as? ProductCell
         //如果cell存在，则不创建，不存在则创建
         if cell == nil {
-            //默认，cell中只能加入一个UIImageView，一个textLabel，和一个指示器
+            
+            cell = NSBundle.mainBundle().loadNibNamed("ProductCell", owner: nil, options: nil).last as? ProductCell
+            
+            
+//            以下注释掉内容为之前使用系统默认的cell时创建cell的方式
+//            默认，cell中只能加入一个UIImageView，一个textLabel，和一个指示器
 //            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: productID)
 //            //可以比默认方式多增加一个subtitle
-            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: productID)
+//            cell = ProductCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: productID)
 //            //比默认方式多增加一个detailTextLabel
 //            cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: productID)
 //            //比默认方式少一个UIImage，
 //            cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: productID)
         }
         
+        //填充数据
         var productInfo = self.productArray[indexPath.row] as! ProductInfo
-        cell?.textLabel?.text = productInfo.title
-        cell?.imageView?.image = UIImage(named: productInfo.icon)
-        cell?.detailTextLabel?.text = productInfo.desc
+        cell?.title.text = productInfo.title
+        cell?.icon.image = UIImage(named: productInfo.icon)
+        cell?.star.image = UIImage(named: productInfo.star)
+        cell?.desc.text = productInfo.desc
+        
+
+//        cell?.textLabel?.text = productInfo.title
+//        cell?.imageView?.image = UIImage(named: productInfo.icon)
+//        cell?.detailTextLabel?.text = productInfo.desc
         return cell!
     }
 }
